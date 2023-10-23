ea1540e20e42853c24be4f710dbe0fa2faf9a99a
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-10-23 06:42:16 -0600

    Update: play Paper Rock Scissors Bo5

--- javascript.js
+++ javascript.js
@@ -1,4 +1,59 @@
 // print rock paper scissors
+console.log('Rock Paper Scissors');
 // player choose
+function getPlayerChoice () {
+  return prompt("Which option would you select (Rock Paper or Scissors)?").toLowerCase()
+}
 // computer choose
+function getComputerChoice () {
+  return getRockPaperScissors(getRandomInt(3));
+}
 // guess who won
+function playRound (playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) return 'No one wins'
+  if (playerSelection == 'rock') {
+    if (computerSelection == 'paper') return 'You lose! Paper beats Rock';
+    return 'You won! Rock beats Scissors'
+  }
+  if (playerSelection == 'paper') {
+    if (computerSelection == 'rock') return 'You won! Paper beats Rock';
+    return 'You lose! Scissors beats Paper';
+  }
+  if (computerSelection == 'rock') return 'You lose! Rock beats Scissors';
+  return 'You won! Scissors beats Paper'
+}
+
+function getRandomInt (max, min = 0) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRockPaperScissors (num) {
+  return num == 0 ? 'rock' 
+    : num == 1 ? 'paper' : 'scissors';
+}
+
+function game () {
+  const ROUNDS = 5;
+  let playerWins = 0;
+  let computerWins = 0;
+  for (let i = 1; i <= ROUNDS; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    const resultRound = playRound(playerSelection, computerSelection)
+    console.log(`Round ${i}: ${resultRound}`);
+    switch (resultRound.charAt(4)) {
+      case 'l':
+        computerWins++;
+        break;
+      case 'w':
+        playerWins++;
+        break;
+    }
+  }
+  console.log(`Bo${ROUNDS} -- Player: ${playerWins} Computer: ${computerWins}`)
+  if (playerWins > computerWins) console.log('You won!');
+  else if (playerWins < computerWins) console.log('You lose!');
+    else console.log("It's a tie!");
+}
+
+game();
