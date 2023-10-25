351cf6eb266cfc6edf318be552ea188b26e0d10f
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-10-25 05:05:31 -0600

    refactor: readability

--- javascript.js
+++ javascript.js
@@ -1,11 +1,3 @@
-// print rock paper scissors
-console.log('Rock Paper Scissors');
-// player choose
-function getPlayerChoice () {
-  const playerChoice = prompt("Which option would you select (Rock Paper or Scissors)?").toLowerCase();
-  if (playerChoice != 'paper' && playerChoice != 'rock' && playerChoice != 'scissors') return getPlayerChoice();
-  return playerChoice;
-}
 // computer choose
 function getComputerChoice () {
   return getRockPaperScissors(getRandomInt(3));
@@ -40,40 +32,48 @@ let playerWins = 0;
 let computerWins = 0;
 
 const listBtnPlayerSelection = document.querySelectorAll('.playerSelection');
-listBtnPlayerSelection.forEach(function (item) {
-  item.addEventListener('click', () => {
-    const resultRound = playRound(item.id, getComputerChoice());
-    const content = document.querySelector('.content');
-    const div = document.createElement('div');
-    div.textContent = `Round ${actualRound + 1}: ${resultRound}`;
-    content.appendChild(div)
-  
-    switch (resultRound.charAt(4)) {
-      case 'l':
-        computerWins++;
-        break;
-      case 'w':
-        playerWins++;
-        break;
-    }
-    actualRound++;
-  
-    if (actualRound === ROUNDS) {
-      const divResult = document.createElement('div');
-      const h2 = document.createElement('h2');
-      const p = document.createElement('p');
-      h2.textContent = `Bo${ROUNDS} -- Player: ${playerWins} Computer: ${computerWins}`;
-      p.textContent = playerWins > computerWins ? 'You won!'
-        : playerWins < computerWins ? 'You lose!'
-        : "It's a tie!";
-      divResult.appendChild(h2);
-      divResult.appendChild(p);
-      content.appendChild(divResult);
-  
-      // end
-      actualRound = 0;
-      playerWins = 0;
-      computerWins = 0;
-    }
+listBtnPlayerSelection.forEach(function (btnPressed) {
+  btnPressed.addEventListener('click', () => {
+    const playerChoice = btnPressed.id;
+    const resultRound = playRound(playerChoice, getComputerChoice());
+    updateScore(resultRound);
+    displayRoundResult (resultRound);
   });
 });
+
+function updateScore (resultRound) {
+  switch (resultRound.charAt(4)) {
+    case 'l':
+      computerWins++;
+      break;
+    case 'w':
+      playerWins++;
+      break;
+  }
+  actualRound++;
+}
+
+function displayRoundResult (resultRound) {
+  const content = document.querySelector('.content');
+  const div = document.createElement('div');
+  div.textContent = `Round ${actualRound}: ${resultRound}`;
+  content.appendChild(div);
+
+  if (actualRound === ROUNDS) {
+    const divResult = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+    h2.textContent = `Bo${ROUNDS} -- Player: ${playerWins} Computer: ${computerWins}`;
+    p.textContent = playerWins > computerWins ? 'You won!'
+      : playerWins < computerWins ? 'You lose!'
+      : "It's a tie!";
+    divResult.appendChild(h2);
+    divResult.appendChild(p);
+    content.appendChild(divResult);
+
+    // end
+    actualRound = 0;
+    playerWins = 0;
+    computerWins = 0;
+  }
+}
