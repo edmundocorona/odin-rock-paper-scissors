595b7ca6114b11c934c518cdb4de81d575babb8a
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-10-25 04:24:05 -0600

    refactor: change querySelector to querySelectorAll
    So it's just one eventListener for all the buttons with class
    playerSelection. It can lead to multiple options i.e.; it can be
    scalable to Rock-Paper-Scissors-Lizard-Spock.

--- javascript.js
+++ javascript.js
@@ -34,15 +34,20 @@ function getRockPaperScissors (num) {
     : num == 1 ? 'paper' : 'scissors';
 }
 
-function gameBo5 () {
-  const ROUNDS = 5;
-  let playerWins = 0;
-  let computerWins = 0;
-  for (let i = 1; i <= ROUNDS; i++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    const resultRound = playRound(playerSelection, computerSelection)
-    console.log(`Round ${i}: ${resultRound}`);
+const ROUNDS = 5;
+let actualRound = 0;
+let playerWins = 0;
+let computerWins = 0;
+
+const listBtnPlayerSelection = document.querySelectorAll('.playerSelection');
+listBtnPlayerSelection.forEach(function (item) {
+  item.addEventListener('click', () => {
+    const resultRound = playRound(item.id, getComputerChoice());
+    const content = document.querySelector('.content');
+    const div = document.createElement('div');
+    div.textContent = `Round ${actualRound + 1}: ${resultRound}`;
+    content.appendChild(div)
+  
     switch (resultRound.charAt(4)) {
       case 'l':
         computerWins++;
@@ -51,125 +56,24 @@ function gameBo5 () {
         playerWins++;
         break;
     }
-  }
-  console.log(`Bo${ROUNDS} -- Player: ${playerWins} Computer: ${computerWins}`)
-  if (playerWins > computerWins) console.log('You won!');
-  else if (playerWins < computerWins) console.log('You lose!');
-    else console.log("It's a tie!");
-}
-
-const ROUNDS = 5;
-let actualRound = 0;
-let playerWins = 0;
-let computerWins = 0;
-
-const btnRock = document.querySelector('#rock');
-btnRock.addEventListener('click', () => {
-  const resultRound = playRound('rock', getComputerChoice());
-  const content = document.querySelector('.content');
-  const div = document.createElement('div');
-  div.textContent = `Round ${actualRound + 1}: ${resultRound}`;
-  content.appendChild(div)
-
-  switch (resultRound.charAt(4)) {
-    case 'l':
-      computerWins++;
-      break;
-    case 'w':
-      playerWins++;
-      break;
-  }
-  actualRound++;
-
-  if (actualRound === ROUNDS) {
-    const divResult = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
-    h2.textContent = `Bo${ROUNDS} -- Player: ${playerWins} Computer: ${computerWins}`;
-    p.textContent = playerWins > computerWins ? 'You won!'
-      : playerWins < computerWins ? 'You lose!'
-      : "It's a tie!";
-    divResult.appendChild(h2);
-    divResult.appendChild(p);
-    content.appendChild(divResult);
-
-    // end
-    actualRound = 0;
-    playerWins = 0;
-    computerWins = 0;
-  }
-});
-
-const btnPaper = document.querySelector('#paper');
-btnPaper.addEventListener('click', () => {
-  const resultRound = playRound('paper', getComputerChoice());
-  const content = document.querySelector('.content');
-  const div = document.createElement('div');
-  div.textContent = `Round ${actualRound + 1}: ${resultRound}`;
-  content.appendChild(div)
-
-  switch (resultRound.charAt(4)) {
-    case 'l':
-      computerWins++;
-      break;
-    case 'w':
-      playerWins++;
-      break;
-  }
-  actualRound++;
-
-  if (actualRound === ROUNDS) {
-    const divResult = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
-    h2.textContent = `Bo${ROUNDS} -- Player: ${playerWins} Computer: ${computerWins}`;
-    p.textContent = playerWins > computerWins ? 'You won!'
-      : playerWins < computerWins ? 'You lose!'
-      : "It's a tie!";
-    divResult.appendChild(h2);
-    divResult.appendChild(p);
-    content.appendChild(divResult);
-
-    // end
-    actualRound = 0;
-    playerWins = 0;
-    computerWins = 0;
-  }
-});
-
-const btnScissors = document.querySelector('#scissors');
-btnScissors.addEventListener('click', () => {
-  const resultRound = playRound('scissors', getComputerChoice());
-  const content = document.querySelector('.content');
-  const div = document.createElement('div');
-  div.textContent = `Round ${actualRound + 1}: ${resultRound}`;
-  content.appendChild(div)
-
-  switch (resultRound.charAt(4)) {
-    case 'l':
-      computerWins++;
-      break;
-    case 'w':
-      playerWins++;
-      break;
-  }
-  actualRound++;
-
-  if (actualRound === ROUNDS) {
-    const divResult = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
-    h2.textContent = `Bo${ROUNDS} -- Player: ${playerWins} Computer: ${computerWins}`;
-    p.textContent = playerWins > computerWins ? 'You won!'
-      : playerWins < computerWins ? 'You lose!'
-      : "It's a tie!";
-    divResult.appendChild(h2);
-    divResult.appendChild(p);
-    content.appendChild(divResult);
-
-    // end
-    actualRound = 0;
-    playerWins = 0;
-    computerWins = 0;
-  }
+    actualRound++;
+  
+    if (actualRound === ROUNDS) {
+      const divResult = document.createElement('div');
+      const h2 = document.createElement('h2');
+      const p = document.createElement('p');
+      h2.textContent = `Bo${ROUNDS} -- Player: ${playerWins} Computer: ${computerWins}`;
+      p.textContent = playerWins > computerWins ? 'You won!'
+        : playerWins < computerWins ? 'You lose!'
+        : "It's a tie!";
+      divResult.appendChild(h2);
+      divResult.appendChild(p);
+      content.appendChild(divResult);
+  
+      // end
+      actualRound = 0;
+      playerWins = 0;
+      computerWins = 0;
+    }
+  });
 });
